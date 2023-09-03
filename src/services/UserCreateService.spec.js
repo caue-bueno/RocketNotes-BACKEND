ceae0cad30ec8430840116ba7faefecad07ba77e
caue-bueno <caue-bueno@live.com>
2023-09-03 14:25:00 -0300

    Added test to check if email already exist when creating new user

--- src/services/UserCreateService.spec.js
+++ src/services/UserCreateService.spec.js
@@ -1,16 +1,45 @@
 const UserCreateService = require('./UserCreateService');
 const UserRepositoryInMemory = require('../repositories/UserRepositoryInMemory');
+const AppError = require("../utils/AppError");
 
-it("user should be created", async () => {
-  const user = {
-    name: "User Test",
-    email: "user@test.com",
-    password: "123"
-  }
 
-  const userRepositoryInMemory = new UserRepositoryInMemory();
-  const userCreateService = new UserCreateService(userRepositoryInMemory);
-  const userCreated = await userCreateService.execute(user);
+describe("UserCreateService", () => {
+  let userRepository = null;
+  let userCreateService = null;
 
-  expect(userCreated).toHaveProperty("id");
+  beforeEach(() => {
+     userRepository = new UserRepositoryInMemory();
+     userCreateService = new UserCreateService(userRepository);
+  });
+
+  it("user should be created", async () => {
+    const user = {
+      name: "User Test",
+      email: "user@test.com",
+      password: "123"
+    }
+
+    const userCreated = await userCreateService.execute(user);
+
+    expect(userCreated).toHaveProperty("id");
+  });
+
+  it("user should not be created with an existing email", async () => {
+    const user1 = {
+      name: "User Test",
+      email: "user@test.com",
+      password: "123"
+    };
+
+    const user2 = {
+      name: "User Test2",
+      email: "user@test.com",
+      password: "123"
+    };
+
+    await userCreateService.execute(user1);
+    await expect(userCreateService.execute(user2)).rejects.toEqual(new AppError("Este e-mail já está cadastrado."))
+
+
+  });
 });
